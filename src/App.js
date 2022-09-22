059be471039f9d44020a151c5648daedcadaeb83
Kevin Song <kevin.kwsong@gmail.com>
2022-09-21 22:14:49 -0400

    added quotations and temporary string

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ export default function App() {
   ))
 
   
+  const anySelected=mods.filter(mod=>mod.selected);
+  const showRegex= anySelected.map(elem=>elem.regex).join("|")
 
-  const showRegex= mods.filter(mod=>mod.selected).map(elem=>elem.regex).join("|")
-  
   function handleClick(id){
     setMods(oldMods=>oldMods.map(mod=>{
       return mod.id === id? {...mod, selected: !mod.selected} :
@@ -29,7 +29,7 @@ export default function App() {
   return (
     <div className="container">
       <Header/>
-      {showRegex}
+      {anySelected.length==0?"Click on the mods which you wish to filter:":`"${showRegex}"`}
       {modElements}
     </div>
   )
